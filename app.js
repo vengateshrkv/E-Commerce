04a9891ced9012baa7b3864f44767a330edfb9e5
vengateshrkv <vengatrkv@gmail.com>
2018-03-20 14:12:01 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
-const express = require("express");
-const app = express();
+const app = require('express')();
+const MongoDB = require("./util/db");
 
-app.use("/products", require("./routes/index"));
-app.listen("3000", "10.100.110.131", () => { });
+app.use('/products', require('./routes/index'));
+
+// Assingning Server Port
+app.listen('3000', '10.100.110.131', () => { });
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
-    var err = new Error("Not Found");
-    err.status = 404;
-    next(err);
+  let err = new Error('Not Found');
+  err.status = 404; // Error status
+  next(err); // Forward error message to error Handler
 });
 
 // error handler
-app.use((err, req, res, next) => {
-    res.send(err.status, err.message);
+app.use((err, req, res) => {
+  res.status(err.status).send(err.message); // Sending error response to client
 });
